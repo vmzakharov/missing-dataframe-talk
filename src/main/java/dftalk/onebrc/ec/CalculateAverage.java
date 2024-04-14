a0c034ded80e0d880d6a9063ddb1a8b30c48e97c
vmzakharov <31494981+vmzakharov@users.noreply.github.com>
2024-04-14 18:25:11 -0400

    adds 1BRC for dataframe-ec and tablesaw

---
+++ src/main/java/dftalk/onebrc/ec/CalculateAverage.java
@@ -0,0 +1,65 @@
+package dftalk.onebrc.ec;
+
+import io.github.vmzakharov.ecdataframe.dataframe.DataFrame;
+import io.github.vmzakharov.ecdataframe.dataset.CsvDataSet;
+import io.github.vmzakharov.ecdataframe.dataset.CsvSchema;
+import io.github.vmzakharov.ecdataframe.util.Stopwatch;
+import org.eclipse.collections.api.factory.Lists;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+
+import static io.github.vmzakharov.ecdataframe.dataframe.AggregateFunction.avg2d;
+import static io.github.vmzakharov.ecdataframe.dataframe.AggregateFunction.max;
+import static io.github.vmzakharov.ecdataframe.dataframe.AggregateFunction.min;
+import static io.github.vmzakharov.ecdataframe.dsl.value.ValueType.FLOAT;
+import static io.github.vmzakharov.ecdataframe.dsl.value.ValueType.STRING;
+
+public class CalculateAverage
+{
+    static private final String MEASUREMENT_PATH = "onebrc";
+    static private final String MEASUREMENT_FILE = "measurements_10.txt";
+
+    public static void main(String[] args) throws Exception
+    {
+        CsvSchema msSchema = new CsvSchema()
+                .addColumn("Station", STRING)
+                .addColumn("Temperature", FLOAT)
+                .separator(';')
+                .hasHeaderLine(false);
+
+        URI measurementFile = CalculateAverage.class.getClassLoader()
+                                                    .getResource(MEASUREMENT_PATH + "/" + MEASUREMENT_FILE)
+                                                    .toURI();
+
+        CsvDataSet msDataSet = new CsvDataSet(Path.of(measurementFile), "measurements", msSchema);
+
+        System.out.println("Loading " + MEASUREMENT_FILE);
+        Stopwatch sw = new Stopwatch();
+        sw.start();
+        DataFrame measurements = msDataSet.loadAsDataFrame();
+        System.out.println(measurements.rowCount());
+        sw.stop();
+
+        System.out.printf("T: %,d, U: %,d, F: %,d\n", sw.totalMemoryBytes(), sw.freeMemoryBytes(), sw.usedMemoryBytes());
+        System.out.printf("Time to load, ms: %,d\n", sw.elapsedTimeMillis());
+
+        sw.start();
+        DataFrame aggregated = measurements
+                .aggregateBy(
+                        Lists.immutable.of(min("Temperature", "Min"), avg2d("Temperature", "Mean"), max("Temperature", "Max")),
+                        Lists.immutable.of("Station"))
+                .sortBy(Lists.immutable.of("Station"));
+        sw.stop();
+        System.out.printf("T: %,d, U: %,d, F: %,d\n", sw.totalMemoryBytes(), sw.freeMemoryBytes(), sw.usedMemoryBytes());
+        System.out.printf("Time to aggregate, ms: %,d\n", sw.elapsedTimeMillis());
+
+        aggregated.forEach(c ->
+                System.out.printf(
+                        "%s=%2.1f/%2.1f/%2.1f\n",
+                        c.getString("Station"), c.getFloat("Min"), c.getDouble("Mean"), c.getFloat("Max")));
+
+//        System.out.println(aggregated.asCsvString());
+    }
+}
---
+++ src/main/java/dftalk/onebrc/tablesaw/CalculateAverage.java
@@ -0,0 +1,64 @@
+package dftalk.onebrc.tablesaw;
+
+import io.github.vmzakharov.ecdataframe.util.Stopwatch;
+import tech.tablesaw.api.ColumnType;
+import tech.tablesaw.api.Table;
+import tech.tablesaw.io.csv.CsvReadOptions;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
+
+import static tech.tablesaw.aggregate.AggregateFunctions.*;
+import static tech.tablesaw.api.ColumnType.*;
+
+public class CalculateAverage
+{
+    static private final String MEASUREMENT_PATH = "onebrc";
+    static private final String MEASUREMENT_FILE = "measurements_10.txt";
+
+    public static void main(String[] args) throws Exception
+    {
+        URL measurementFile = CalculateAverage.class.getClassLoader().getResource(MEASUREMENT_PATH + "/" + MEASUREMENT_FILE);
+
+        CsvReadOptions options = CsvReadOptions
+                .builder(measurementFile)
+                .columnTypes(new ColumnType[] {STRING, FLOAT})
+                .separator(';')
+                .header(false)
+                .build();
+
+        System.out.println("Loading " + MEASUREMENT_FILE);
+
+        Stopwatch sw = new Stopwatch();
+
+        sw.start();
+        Table measurements = Table.read().usingOptions(options);
+
+        measurements.column(0).setName("Station");
+        measurements.column(1).setName("Temperature");
+        System.out.println(measurements.rowCount());
+        sw.stop();
+
+        System.out.printf("T: %,d, U: %,d, F: %,d\n", sw.totalMemoryBytes(), sw.freeMemoryBytes(), sw.usedMemoryBytes());
+        System.out.printf("Time to load, ms: %,d\n", sw.elapsedTimeMillis());
+
+        sw.start();
+        Table aggregated = measurements
+                .summarize("Temperature", min, mean, max).by("Station")
+                .sortOn("Station");
+        sw.stop();
+
+        System.out.printf("T: %,d, U: %,d, F: %,d\n", sw.totalMemoryBytes(), sw.freeMemoryBytes(), sw.usedMemoryBytes());
+        System.out.printf("Time to aggregate, ms: %,d\n", sw.elapsedTimeMillis());
+
+        aggregated.forEach(c ->
+                System.out.printf(
+                        "%s=%2.1f/%2.1f/%2.1f\n",
+                        c.getString("Station"),
+                        c.getDouble("Min [Temperature]"),
+                        c.getDouble("Mean [Temperature]"),
+                        c.getDouble("Max [Temperature]"))
+        );
+    }
+}
