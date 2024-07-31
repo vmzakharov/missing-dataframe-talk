e284287300bba2cce3cd803efc84063da0e678af
vmzakharov <31494981+vmzakharov@users.noreply.github.com>
2024-07-31 17:41:36 -0400

    adds unit test configuration for Kotlin tests

---
+++ src/test/kotlin/dftalk/donuts/dfkotlin/DonutDataFrameKotlinTest.kt
@@ -0,0 +1,19 @@
+package dftalk.donuts.dfkotlin
+
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class DonutDataFrameKotlinTest {
+    @BeforeTest
+    fun setUpDonuts() {
+        println("BeforeTest")
+
+    }
+
+    @Test
+    fun testBleepBloop()
+    {
+        assertTrue(true);
+    }
+}
